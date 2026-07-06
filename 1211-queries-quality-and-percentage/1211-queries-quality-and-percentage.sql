cd53eb0c7d9775ee344fb52c1785cad93fa27f7a
Ujala Saroj <Ujalasaroj9968@gmail.com>
2026-07-06 22:14:17 +0530

    Time: 354 ms (89.44%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte as 
+(select query_name , rating/position as ratio , case when rating < 3 then 1 else 0 end as qulaity_bin
+from Queries)
+
+
+select query_name , round(avg(ratio),2) as quality , round(sum(qulaity_bin)/count(*) * 100,2) as poor_query_percentage
+from cte 
+group by query_name
